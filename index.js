2946e908034ca182a1ca5a5aa58e6419fb5da45a
michelle <junemichelle20@gmail.com>
2023-09-29 19:37:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+console.log(document)
+console.log(typeof document)
+
+let a=document.getElementById("main")
+//console.log(a)
+let b=a.remove("main")
+//console.log(b)
+
+let c=document.createElement("h1")
+console.log(c)
+c.setAttribute("id","victory")
+let newHeader=document.body.appendChild(c)
+newHeader.textContent="YOUR-NAME is the champion"
+
+
+
